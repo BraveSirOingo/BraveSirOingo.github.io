af088794a0619c9f6e0e612035e4fa8ed250860c
BraveSirOingo <joshrooney@gmail.com>
2021-02-01 22:35:43 -0400

    The First Commit of the Casey Project

---
+++ javascript.js
@@ -0,0 +1,28 @@
+var love = [
+    'I love you Casey Bear.',
+    'I think you\'re amazing!',
+    'I love Casey so much!',
+    'If I had to choose to be a millionaire or be with Casey I\'d be a millionaire and then also be with Casey.',
+    'Shall I compare thee to a summer day? (nah, you\'re more rad than a summer day.',
+    'You have the sweetest buns.',
+    'You have the most lovely kisses!',
+    'I wish I was with you right now giving you a smooch.',
+    'You look cute in those glasses.',
+    'I really like getting up and making you coffee while you get ready.',
+    'You can probably do anything if you wanted to.',
+    'Gizmo.',
+    'You smell so nice!',
+    'GIZMO!',
+    'You got so good at ukulele so quickly!',
+    'How are you so sweet?',
+    'Do you need anything darling?',
+    'WHERE IS ANDY FILMORE?!',
+    'You are SO KIND to people!',
+
+]
+
+function newLove() {
+    var randomNumber = Math.floor(Math.random()*(love.length));
+    document.getElementById('loveDisplay').innerHTML = love[randomNumber];
+}
+
